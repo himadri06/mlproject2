7fcc6a21ede1e1cfda5bbc241c46b758b96f6d99
himadri06 <hbhowmick91@gmail.com>
2024-05-06 17:20:38 +0530

    Data transformation done

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from src.mlproject2.logger import logging
 from src.mlproject2.exception import CustomException
 from src.mlproject2.components.data_ingestion import DataIngestionConfig
 from src.mlproject2.components.data_ingestion import DataIngestion
+from src.mlproject2.components.data_transformation import DataTransformationConfig
+from src.mlproject2.components.data_transformation import DataTransformation
 
 import sys
 
@@ -11,7 +13,10 @@ if __name__=='__main__':
     try:
         #data_ingestion_config=DataIngestionConfig()
         DataIngestion=DataIngestion()
-        DataIngestion.initiate_data_ingestion()
+        train_data_path,test_data_path=DataIngestion.initiate_data_ingestion()
+        #data_transformation_config=DataTransformationConfig()
+        data_transformation=DataTransformation()
+        train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data_path,test_data_path)
     except Exception as e:
         logging.info("Raise Custom Exception")
         raise CustomException(e,sys)
--- src/mlproject2/components/data_ingestion.py
+++ src/mlproject2/components/data_ingestion.py
@@ -21,7 +21,7 @@ class DataIngestion:
 
     def initiate_data_ingestion(self):
         try:
-            df=read_sql_data()
+            df=pd.read_csv('data\stud.csv')
             logging.info("Reading from MYSQL db")
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
